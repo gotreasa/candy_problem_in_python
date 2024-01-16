a9a9b8106de12ff3e38010d7ce268e01d7bdcc44
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-16 12:26:47 +0000

    refactor: programmatically calculate the number of candies to be given out

--- modules/candy_problem.py
+++ modules/candy_problem.py
@@ -1,8 +1,8 @@
 def candies(number_candies: list[int]) -> int:
-    if number_candies == [5, 8, 6, 4]:
-        return 9
-    if number_candies == [1, 2, 4, 6]:
-        return 11
-    if number_candies == [1, 6]:
-        return 5
-    raise ValueError("❗️ Input should be a list of integers")
+    if any(not isinstance(child_candies, int) for child_candies in number_candies):
+        raise ValueError("❗️ Input should be a list of integers")
+    required_candies = max(number_candies)
+    result = 0
+    for child_number in number_candies:
+        result += required_candies - child_number
+    return result
