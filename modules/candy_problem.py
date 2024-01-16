553a6730beb8e796fedc1641e51b20f3f6cd800b
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-16 12:33:11 +0000

    refactor: make the check for one or no child more obvious

--- modules/candy_problem.py
+++ modules/candy_problem.py
@@ -1,7 +1,13 @@
+def is_one_or_no_child(number_of_children: list[int]) -> bool:
+    if len(number_of_children) <= 1:
+        return True
+    return False
+
+
 def candies(number_candies: list[int]) -> int:
     if any(not isinstance(child_candies, int) for child_candies in number_candies):
         raise ValueError("❗️ Input should be a list of integers")
-    if len(number_candies) <= 1:
+    if is_one_or_no_child(number_candies):
         return -1
     required_candies = max(number_candies)
     result = 0
