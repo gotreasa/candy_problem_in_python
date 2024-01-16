2047327fbc5f66851bd9ceb5640858a2e8f2e464
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-16 12:15:12 +0000

    feat: should give out  9 when the list of children candies are (5, 8, 6, 4)

--- modules/candy_problem.py
+++ modules/candy_problem.py
@@ -1,2 +1,4 @@
 def candies(number_candies: int) -> int:
+    if number_candies == [5, 8, 6, 4]:
+        return 9
     raise ValueError("❗️ Input should be a list of integers")
--- tests/unit/test_0_installer_runner.py
+++ tests/unit/test_0_installer_runner.py
@@ -7,3 +7,7 @@ def describe_candies():
         """🧪 should error when a list of numbers is not received"""
         with pytest.raises(ValueError, match="❗️ Input should be a list of integers"):
             candy_problem.candies("blah")
+
+    def should_give_out_9_candies_for_5_8_6_4():
+        """🧪 should give out 9 when the list of children candies are (5, 8, 6, 4)"""
+        assert candy_problem.candies([5, 8, 6, 4]) == 9
